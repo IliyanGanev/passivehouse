66a9737db607372532b4e4df0a3078cebf7f80e0
Iliyan Ganev <ganeviliyan@gmail.com>
2018-03-08 12:07:36 -0800

    Add mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,21 @@
 
 
 User.create!(name:  "Example User",
-             email: "example@passivehouse.org",
+             email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             
+             activated: true,
+             activated_at: Time.zone.now)
 
-100.times do |n|
+99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@passivehouse.org"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
