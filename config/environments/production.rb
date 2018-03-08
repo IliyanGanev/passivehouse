39f3ea6efd7a61c211e080c2a5d0840816d3044c
Iliyan Ganev <ganeviliyan@gmail.com>
2018-03-08 12:31:32 -0800

    Add host to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'passivehousedetails.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -100,8 +100,8 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'passivehousedetails.herokuapp.com',
+    :domain         => 'herokuapp.com',
     :enable_starttls_auto => true
   }
-  
+
 end
