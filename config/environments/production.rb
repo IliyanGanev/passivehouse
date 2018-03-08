bc6418a60cfbf0806485104d2a5b3036f3e28d82
Iliyan Ganev <ganeviliyan@gmail.com>
2018-03-08 12:20:00 -0800

    test sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'passivehousedetails.herokuapp.com',
     :enable_starttls_auto => true
   }
   
