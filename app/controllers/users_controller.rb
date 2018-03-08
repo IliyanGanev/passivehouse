09915f1a1b8110ceb3b6f7ed81a3cba9e72da6aa
Iliyan Ganev <ganeviliyan@gmail.com>
2018-03-07 23:10:07 -0800

    Advanced login with persostent session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)   
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to PassiveHouseDetails!"
       # redirect_to user_url(@user)
       redirect_to @user
