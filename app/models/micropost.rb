b51619897f567841e7d878a848ad0a48d3271575
Iliyan Ganev <ganeviliyan@gmail.com>
2018-03-08 22:31:16 -0800

    Picture uploader up and running

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,18 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :content, length: { maximum: 2000 }
+  validates :content, presence: true, length: { maximum: 2000 }
   validates :tag, presence: true
+  validate :picture_size
+
+  private
+
+  # Custom validator for the size of an uploaded picture.
+  def picture_size
+    if picture.size > 5.megabytes 
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309011130) do
+ActiveRecord::Schema.define(version: 20180309053025) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180309011130) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
