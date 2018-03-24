2b10cd9539f1a2ad7e606e72797cb6faddd919bd
Iliyan Ganev <ganeviliyan@gmail.com>
2018-03-24 12:49:26 -0700

    Add some js to resize the anime on window resize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,24 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+
+	$(window).resize(function (e) {
+		if (window.resizeAnimationTimeout != null) {
+			window.clearTimeout(window.resizeAnimationTimeout);
+		}
+		window.resizeAnimationTimeout = window.setTimeout(function () {
+			console.log('asdfasdfsadfsfsddfsdfdsf')
+			var w = $(window);
+			startAnimation(
+				document.getElementById("home"),
+				w.width(),
+				w.height());
+		}, 200);
+	});
+
+	window.resizeAnimationTimeout = null;
+	
+});
