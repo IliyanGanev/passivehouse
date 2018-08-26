561cb775d33e017e9671e6e4aed124fd0ed38787
Iliyan Ganev <ganeviliyan@gmail.com>
2018-08-26 14:53:17 -0700

    Comment out the first page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,16 @@
 class StaticPagesController < ApplicationController
   
   def home
-    if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
 
-      @microposts = Micropost.all.paginate(page: params[:page])
+    render html: '<p>nothing</p>'.html_safe
+    # if logged_in?
+    #   @micropost  = current_user.microposts.build
+    #   @feed_items = current_user.feed.paginate(page: params[:page])
+    # end
+
+    #   @microposts = Micropost.all.paginate(page: params[:page])
      
-       @users = User.where(activated: true).paginate(page: params[:page])
+    #    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def help
