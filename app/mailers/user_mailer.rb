d024ead0e508399305e8ce93e657e0a37bbd869b
Iliyan Ganev <ganeviliyan@gmail.com>
2018-09-23 17:40:02 -0700

    asdf

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,6 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.account_activation.subject
   #
   def account_activation(user)
-    p "account-activation$$$" * 30
     @user = user
     mail to: user.email, subject: "Account activation"
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'passivehousedetails.herokuapp.com'
+  # host = 'passivehousedetails.herokuapp.com'
+   host = 'passivehousedetails.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
