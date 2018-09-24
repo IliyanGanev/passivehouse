38a357bf43ecf553e6a9552eb4b2d3a69a994015
Iliyan Ganev <ganeviliyan@gmail.com>
2018-09-23 17:41:50 -0700

    asdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   # host = 'passivehousedetails.herokuapp.com'
-   host = 'passivehousedetails.herokuapp.com'
+  host = 'passivehousedetails.org'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
